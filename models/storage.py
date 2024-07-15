6d7836f2d1b574b2b82f7d1e02a0d034aac9ab0c
David <obododavidsop@gmail.com>
2024-07-15 10:04:09 +0100

    storage

--- models/storage.py
+++ models/storage.py
@@ -6,8 +6,6 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-engine = create_engine("sqlite:///:memory:", echo=True)
-
 
 class Storage:
     """Represents a storage class"""
@@ -22,9 +20,28 @@ class Storage:
     def load_session(self) -> None:
         """Load database session"""
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=engine, expire_on_commit=False)
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False
+        )
         Session = scoped_session(session_factory)
-        self.__session = Session
+        self.__session = Session\
+
+    def add(self, obj):
+        """Adds an object to Storage"""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commit changes to storage"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Deletes an object from storage"""
+        if obj is not None:
+            if isinstance(obj, list):
+                for instance in obj:
+                    self.__session.delete(instance)
+            else:
+                self.__session.delete(obj)
 
     def close(self) -> None:
         """Close the session"""
